514681f47b1cb161e8f60b9f90fd9c12e142b489
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-17 06:18:58 -0500

    Add Lesson_2_Building_Your_First_Vue.Js_Application/part_7_AppWithMessageComponent.js

---
+++ Lesson_2_Building_Your_First_Vue.Js_Application/part_7_AppWithMessageComponent.js
@@ -0,0 +1,9 @@
+import MessageComponent from './components/MessageComponent.vue'
+components: {
+  MessageComponent
+}
+<template>
+  <div id="app">
+    <MessageComponent initialMessage="Hello from the App Component!" />
+  </div>
+</template>
