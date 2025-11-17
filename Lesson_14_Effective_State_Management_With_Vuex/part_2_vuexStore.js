5928724e17f42435c7b764740b44c6ab8d4d9a14
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-17 06:16:27 -0500

    Add Lesson_14_Effective_State_Management_With_Vuex/part_2_vuexStore.js

---
+++ Lesson_14_Effective_State_Management_With_Vuex/part_2_vuexStore.js
@@ -0,0 +1,14 @@
+// store.js
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    todos: [],
+  }
+});
+
+export default store;
