710c3dfb53b5e6ed728925891079a4dd73c1f8be
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-17 06:17:37 -0500

    Add Lesson_21_Enhancing_Application_Interactivity_With_Vue.Js/part_4_CounterComponent.ts

---
+++ Lesson_21_Enhancing_Application_Interactivity_With_Vue.Js/part_4_CounterComponent.ts
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Current Count: {{ count }}</h1>
+    <h2>{{ countMessage }}</h2>
+    <button @click="increment">Increment</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+
+export default defineComponent({
+  name: 'Counter',
+  setup() {
+    const count = ref(0);
+    
+    function increment() {
+      count.value++;
+    }
+    
+    function reset() {
+      count.value = 0;
+    }
+
+    const countMessage = computed(() => {
+      return count.value % 2 === 0 ? 'Even' : 'Odd';
+    });
+
+    return {
+      count,
+      countMessage,
+      increment,
+      reset
+    };
+  }
+});
+</script>
