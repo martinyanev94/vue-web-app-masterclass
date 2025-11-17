4445e7744f71246ea264b02bcc071c2e65b91507
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-17 06:19:44 -0500

    Add Lesson_6_Using_Vue.Js_Devtools_For_Debugging/part_6_store.js.js

---
+++ Lesson_6_Using_Vue.Js_Devtools_For_Debugging/part_6_store.js.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    }
+  }
+});
+
+export default store;
