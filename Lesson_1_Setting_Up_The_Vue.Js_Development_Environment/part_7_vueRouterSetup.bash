59c8bd94c4fbd77111e53f1415065b075556d74d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-17 06:17:26 -0500

    Add Lesson_1_Setting_Up_The_Vue.Js_Development_Environment/part_7_vueRouterSetup.bash

---
+++ Lesson_1_Setting_Up_The_Vue.Js_Development_Environment/part_7_vueRouterSetup.bash
@@ -0,0 +1,23 @@
+npm install vue-router
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
